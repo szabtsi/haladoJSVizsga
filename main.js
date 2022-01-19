93724a4b72f62f26637d79a4a63071e99d83e424
Felföldi Szabolcs <szabolcs.felfoldi10@gmail.com>
2022-01-19 18:32:34 +0100

    findOldestCar() fgv: legöregebb autó megkeresése

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ class Project {
         this.searchField = document.getElementById('searchField');
         this.searchCarBrandBtn = document.getElementById('searchCarBrandBtn');
         this.resultTbody = document.getElementById('resultTbody');
+
+        this.oldestCarBtn.onclick = this.findOldestCar;
     }
 
     requestCars = async () => {
@@ -27,9 +29,29 @@ class Project {
                 <td>${carData.brand}</td>
                 <td>${carData.type}</td>
                 <td>${carData.factoryYear}</td>
-            </tr>`
+            </tr>`;
         }
         this.resultTbody.innerHTML = resultHTML;
+    }
+
+    findOldestCar = async () => {
+        let cars = await this.requestCars();
+
+        let minYear = cars[0].factoryYear;
+        for(let i=1; i<cars.length; i++) {
+            if(cars[i].factoryYear < minYear) {
+                minYear = cars[i].factoryYear;
+            }
+        }
+        
+        let result = [];
+        for(let carData of cars) {
+            if(carData.factoryYear == minYear) {
+                result.push(carData);
+            }
+        }
+        
+        this.putCarsToTable(result);
     }
 }
 
