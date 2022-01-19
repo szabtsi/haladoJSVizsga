e1eeef3bb527b517de8e7d7dbe4e85efc3fe8722
Felföldi Szabolcs <szabolcs.felfoldi10@gmail.com>
2022-01-19 17:57:10 +0100

    requestCars függvény HTTP kérések bonyolítása és válaszok feldolgozása

--- main.js
+++ main.js
@@ -12,6 +12,12 @@ class Project {
         this.searchCarBrandBtn = document.getElementById('searchCarBrandBtn');
         this.resultTbody = document.getElementById('resultTbody');
     }
+
+    requestCars = async () => {
+        let respond = await fetch('cars.json');
+        let cars = await respond.json();
+        return cars;
+    }
 }
 
 const projectObject = new Project();
